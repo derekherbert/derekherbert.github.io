dc0bd79ac05fde5ebbd589cdcedbcb3097f259ea
Derek Herbert <derek.herbert@cleargoals.com>
2019-08-17 19:43:11 -0400

    Carousel working fairly well. A few visual kinks to work out still

--- js/header.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    
-    
-    $( "#header_dropdown_button" ).click( function() {
-        $("#header_dropdown_row").toggleClass("hide");
-        $(".header-nav-bar").toggleClass("square-bottoms");
-    });
-});
-
---
+++ js/main.js
@@ -0,0 +1,216 @@
+//Global Variables
+var carouselIndex = 0;
+var repos = [
+                { name:"derekherbert.github.io", img:"portfolio.png",     overlay:"Portfolio"                  },
+                { name:"BambooTempleBookStore",  img:"bamboo_temple.jpg", overlay:"Bamboo Temple eBook Store"  }, //1st visible repo 
+                { name:"JAG",                    img:"jag.png",           overlay:"Java Application for GMail" }, //2nd visible repo
+                { name:"TravelACDZ",             img:"acdz.png",          overlay:"Travel ACDZ Android App"    }, //3rd visible repo
+                { name:"derekherbert.github.io", img:"portfolio.png",     overlay:"Portfolio"                  }, //4rd visible repo
+                { name:"BambooTempleBookStore",  img:"bamboo_temple.jpg", overlay:"Bamboo Temple eBook Store"  },
+                { name:"JAG",                    img:"jag.png",           overlay:"Java Application for GMail" },
+                { name:"TravelACDZ",             img:"acdz.png",          overlay:"Travel ACDZ Android App"    },
+                { name:"TravelACDZ",             img:"acdz.png",          overlay:"Travel ACDZ Android App"    }
+                
+            ];
+var imgPath = './images/';
+var githubPath = 'https://github.com/derekherbert/';
+
+
+
+//Event Listeners
+$(document).ready(function() { 
+    prepareCarousel(); 
+    $('#carousel-left button').click(function() { carouselLeft(); });
+    $('#carousel-right button').click(function() { carouselRight(); });
+});    
+
+
+
+$( "#header_dropdown_button" ).click( function() {
+    $("#header_dropdown_row").toggleClass("hide");
+    $(".header-nav-bar").toggleClass("square-bottoms");
+});
+
+function prepareCarousel() 
+{
+    $('.overlay-container').each(function(index) { updateRepo($(this), index); });
+    $(repos).each(function(index) 
+        {
+            (index < 4) ? $('#carousel-index').append('<span class="active-bullet">&bull;</span>') 
+                                       : $('#carousel-index').append('<span>&bull;</span>');
+        }
+    );
+}
+
+function updateRepo(repo, index)
+{
+    repo.find('img').attr('src', imgPath + repos[index].img);
+    repo.find('a').attr('href', githubPath + repos[index].name);
+    repo.find('.overlay-text').text(repos[index].overlay);
+    repo.attr('repoIndex', index);
+}
+
+function updateCarouselIndex() 
+{
+    var visibleOverlays = $($('.overlay-container').slice(1, 5));
+    var bullets = $('#carousel-index span');
+
+    bullets.removeClass('active-bullet');
+
+    visibleOverlays.each(function() 
+        {
+            var repoIndex = (parseInt($(this).attr('repoIndex')));
+            (repoIndex == 0) ? $(bullets[bullets.length - 1]).addClass('active-bullet') : $(bullets[repoIndex - 1]).addClass('active-bullet');
+        }
+    );
+}
+
+function carouselRight() 
+{
+    var allOverlays = $('.overlay-container');
+    var activeOverlays = $(allOverlays.slice(1, 6)); //Only the last 5 overlays have animations
+    var invisibleRightOverlay = $(activeOverlays[4]);
+    var colWidth = invisibleRightOverlay.parent().css('width');
+
+    //Disable carousel-left button until animation finishes
+    $('#carousel-left button').prop('disabled', true);
+
+    //Disable overlay:hover animation until carousel animation finishes
+    $('.overlay:hover').css('opacity', 0);
+
+    //Fade in the invisible item on the right of the carousel
+    invisibleRightOverlay.removeClass('visibility-hidden');
+    invisibleRightOverlay.fadeOut(0); //Fade it out so it can be faded in
+    invisibleRightOverlay.fadeIn(500);
+
+    //Fade out the last visible item on the left of the carousel
+    $(activeOverlays[0]).fadeOut(500);
+
+    //Carousel left animation
+    activeOverlays.animate( { left: '-' + colWidth }, { duration: 1000, queue: false }, 'swing', function(){} );
+
+    //Carousel rotation animation 
+    activeOverlays.animateRotate(0, -15, 500, 'swing', function() //Swing counter-clockwise 15 degrees
+        {
+            activeOverlays.animateRotate(-15, 0, 500, 'swing');   //Swing clockwise 15 degrees (back to normal)
+        }
+    );
+
+    //Move animated elements back to their original position and update DOM
+    setTimeout(function() 
+        {
+            //Reset the activeOverlays to their original positions
+            activeOverlays.css('left', '');
+
+            //Fade out the right-most repo instantly
+            $(activeOverlays[4]).addClass('visibility-hidden');
+
+            //Fade in the left-most visible repo instantly
+            $(activeOverlays[0]).fadeIn(0);
+                                    
+            //Update each icon with new repo[repoIndex] to match what the animation did
+            allOverlays.each(function() 
+                {
+                    var repoIndex = parseInt($(this).attr('repoIndex'));
+                    (repoIndex == (repos.length - 1)) ? updateRepo($(this), 0) : updateRepo($(this), repoIndex + 1);
+                }
+            );
+
+            //Enable carousel-right button now that the animation is finished
+            $('#carousel-left button').prop('disabled', false);
+
+            //Update carousel index to match animation
+            updateCarouselIndex();
+
+        }, 1025); //Starts 25ms after the carousel animation completes
+
+    //Avoid any glitchy hover effects during the carousel animation
+    setTimeout(function() 
+        {
+            //Enable overlay:hover animation now that the carousel animation is finished
+            $('.overlay:hover').css('opacity', 1);
+
+        }, 1450); //Starts 450ms after the carousel animation completes
+}
+
+function carouselLeft() 
+{
+    var allOverlays = $('.overlay-container');
+    var activeOverlays = $(allOverlays.slice(0, 5)); //Only the first 5 overlays have animations
+    var invisibleLeftOverlay = $(activeOverlays[0]);
+    var colWidth = invisibleLeftOverlay.parent().css('width');
+
+    //Disable carousel-right button until animation finishes
+    $('#carousel-right button').prop('disabled', true);
+
+    //Disable overlay:hover animation until carousel animation finishes
+    $('.overlay:hover').css('opacity', 0);
+
+    //Fade in the invisible item on the left of the carousel
+    invisibleLeftOverlay.removeClass('visibility-hidden');
+    invisibleLeftOverlay.fadeOut(0); //Fade it out so it can be faded in
+    invisibleLeftOverlay.fadeIn(500);
+
+    //Fade out the last visible item on the right of the carousel
+    $(activeOverlays[4]).fadeOut(500);
+
+    //Carousel left animation
+    activeOverlays.animate( { left: colWidth }, { duration: 1000, queue: false }, 'swing', function(){} );
+
+    //Carousel rotation animation 
+    activeOverlays.animateRotate(0, 15, 500, 'swing', function() //Swing clockwise 15 degrees
+        {
+            activeOverlays.animateRotate(15, 0, 500, 'swing');   //Swing counter-clockwise 15 degrees (back to normal)
+        }
+    );
+
+    //Move animated elements back to their original position and update DOM
+    setTimeout(function() 
+        {
+            //Reset the activeOverlays to their original positions
+            activeOverlays.css('left', '');
+
+            //Fade out the left-most repo instantly
+            $(activeOverlays[0]).addClass('visibility-hidden');
+
+            //Fade in the right-most visible repo instantly
+            $(activeOverlays[4]).fadeIn(0);
+                                    
+            //Update each repo visually to match the animation
+            allOverlays.each(function() 
+                {
+                    var repoIndex = parseInt($(this).attr('repoIndex'));
+                    (repoIndex == 0) ? updateRepo($(this), (repos.length - 1)) : updateRepo($(this), repoIndex - 1);
+                }
+            );
+
+            //Enable carousel-right button now that the animation is finished
+            $('#carousel-right button').prop('disabled', false);
+
+            //Update carousel index to match animation
+            updateCarouselIndex();
+
+        }, 1025); //Starts 25ms after the carousel animation completes
+    
+    //Avoid any glitchy hover effects during the carousel animation
+    setTimeout(function() 
+        {
+            //Enable overlay:hover animation now that the carousel animation is finished
+            $('.overlay:hover').css('opacity', 1);
+
+        }, 1450); //Starts 450ms after the carousel animation completes
+}
+
+$.fn.animateRotate = function(startAngle, endAngle, duration, easing, complete) {
+    var args = $.speed(duration, easing, complete);
+    var step = args.step;
+    return this.each(function(i, e) {
+      args.complete = $.proxy(args.complete, e);
+      args.step = function(now) {
+        $.style(e, 'transform', 'rotate(' + now + 'deg)');
+        if (step) return step.apply(e, arguments);
+      };
+  
+      $({deg: startAngle}).animate({deg: endAngle}, args);
+    });
+};
